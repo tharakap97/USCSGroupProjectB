c64191c70a4406520ad1d5bbe9680df081ab9b93
Paba Yashodha <pabayashodha@gmail.com>
2026-03-28 17:23:51 +0530

    add signin page

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -1,5 +1,6 @@
 package pages;
 
+import org.apache.commons.math3.ode.nonstiff.HighamHall54FieldIntegrator;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
@@ -7,6 +8,7 @@ import org.openqa.selenium.support.PageFactory;
 
 public class HomePage extends BasePage{
     public static final By JOIN_BTN = By.xpath("//a[text()='Join']");
+    private static final By HOME_SIGNIN_BUTTON= By.xpath("//a[text()='Sign in']");
 
     public HomePage(WebDriver driver) {
         super(driver);
@@ -17,4 +19,11 @@ public class HomePage extends BasePage{
         click(JOIN_BTN);
         return PageFactory.initElements(driver, JoinPage.class);
     }
+
+    public  SignInPage selectSignIn(){
+        locatorWaiting(HOME_SIGNIN_BUTTON);
+        click(HOME_SIGNIN_BUTTON);
+        return PageFactory.initElements(driver, SignInPage.class);
+    }
+
 }
--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,5 +1,6 @@
 package tests;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.PageFactory;
@@ -13,7 +14,8 @@ public class BaseTest {
 
     @BeforeClass
     public void setupBrowser(){
-        driver = new ChromeDriver();
+        //driver = new ChromeDriver();
+        driver= WebDriverManager.chromedriver().create();
         driver.manage().window().maximize();
         BasePage = PageFactory.initElements(driver, BasePage.class);
     }
