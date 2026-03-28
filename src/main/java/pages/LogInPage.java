32651efe8ed84b60c43ff63c0b6b64d3aa09a73f
Dulki Dilshara <dilsharadulki@gmail.com>
2026-03-28 10:34:07 +0530

    Add BaseTest file

--- src/main/java/pages/LogInPage.java
+++ src/main/java/pages/LogInPage.java
@@ -1,8 +1,5 @@
 package pages;
 
-import org.openqa.selenium.By;
-
 public class LogInPage {
 
-    private static final By TF_EMAIL = By.xpath("//label[text()='Email']::div/child::input");
 }
---
+++ src/test/java/tests/BaseTest.java
@@ -0,0 +1,20 @@
+package tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.BeforeClass;
+import pages.BasePage;
+
+public class BaseTest {
+
+    protected BasePage BasePage;
+    protected WebDriver driver;
+
+    @BeforeClass
+    public void setupBrowser(){
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        BasePage = PageFactory.initElements(driver, BasePage.class);
+    }
+}
