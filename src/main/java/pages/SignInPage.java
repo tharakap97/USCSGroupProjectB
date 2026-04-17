04d9631819bfa6a7ce89bd66d9f4820accb47e51
pererahiru <pererahiruni.k@gmail.com>
2026-04-17 21:01:50 +0530

    Changes of SignInTest and SignInPage

--- src/main/java/pages/SignInPage.java
+++ src/main/java/pages/SignInPage.java
@@ -9,20 +9,50 @@ public class SignInPage extends BasePage {
         super(driver);
     }
 
-    private static final By LOGIN_EMAIL = By.xpath("//input[@id='email']");
-    private static final By LOGIN_PASSWORD= By.xpath("//input[@id='password']");
-    private static final By LOGIN_SIGNIN_BUTTON= By.xpath("//button[@type='submit']");
-    private static final By LOGIN_CREATE_ACCOUNT= By.xpath("//a[text()='Create account']");
+    public static final By LOGIN_EMAIL = By.xpath("//input[@id='email']");
+    public static final By LOGIN_PASSWORD = By.xpath("//input[@id='password']");
+    public static final By LOGIN_SIGNIN_BUTTON = By.xpath("//button[@type='submit']");
+    public static final By LOGIN_CREATE_ACCOUNT = By.xpath("//a[text()='Create account']");
+
+    public static final By LOGIN_REMEMBER_MY_CREDENTIALS =  By.xpath("//input[@type=\"checkbox\"]");
+    public static final By LOGIN_SHOW_BUTTON = By.xpath("//button[text()='Show']");
+    public static final By LOGIN_HIDE_BUTTON = By.xpath("//button[text()='Hide']");
+    public static final By LOGOUT = By.xpath("//button[text()='Logout']");
+    //Logout button is not on this page... Maybe we have to move it to dashboard page
 
 
-
-
-    public void typeEmail(String text){
-        driver.findElement(LOGIN_EMAIL).sendKeys(text);
+    public void typeText(By locator, String text) {
+        type(locator, text);
     }
-    public void typePassword(String text){
-        driver.findElement(LOGIN_PASSWORD).sendKeys(text);
+
+    public void clickRememberMyCredentials(){
+        locatorWaiting(LOGIN_REMEMBER_MY_CREDENTIALS);
+        click(LOGIN_REMEMBER_MY_CREDENTIALS);
     }
+
+    public void clickShowButton(){
+        locatorWaiting(LOGIN_SHOW_BUTTON);
+        click(LOGIN_SHOW_BUTTON);
+    }
+    public void clickHideButton() {
+        locatorWaiting(LOGIN_HIDE_BUTTON);
+        click(LOGIN_HIDE_BUTTON);
+    }
+
+    public void clickSignIn() {
+        locatorWaiting(LOGIN_SIGNIN_BUTTON);
+        click(LOGIN_SIGNIN_BUTTON);
+    }
+
+    public void clickCreateAcc() {
+        locatorWaiting(LOGIN_CREATE_ACCOUNT);
+        click(LOGIN_CREATE_ACCOUNT);
+    }
+
+    public void clickLogout() {
+        locatorWaiting(LOGOUT);
+        click(LOGOUT);
+    } //If Logout xpath moved to DashboardPage, move this method there...
 
 
 }
--- src/test/java/tests/signIn/SignInTest.java
+++ src/test/java/tests/signIn/SignInTest.java
@@ -1,25 +1,93 @@
 package tests.signIn;
 
+import org.apache.xmlbeans.impl.xb.xsdschema.Public;
+import org.openqa.selenium.By;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import pages.HomePage;
 import pages.SignInPage;
 import tests.BaseTest;
 
+import javax.lang.model.element.Name;
+
 public class SignInTest extends BaseTest {
     private SignInPage signInPage;
 
     @BeforeMethod
-    public void verifyJoinPage() {
+    public void verifyJoinPage(){
         HomePage homePage = BasePage.initApp();
         signInPage = homePage.selectSignIn();
     }
 
-    @Test
-    public void successLogin() throws InterruptedException {
-        //Thread.sleep(5000);
-        signInPage.typeEmail("yashguruge@gmail.com");
-       // Thread.sleep(5000);
-        signInPage.typePassword("");
+//    If Logout xpath moved to Dashboard page, move this test to DashboardTet
+//    @Test (dependsOnMethods = "successfulLogin")
+//    public void logOut(){
+//        signInPage.clickLogout();
+
+    @Test (priority = 1)
+    public void emptyEmailAndPasswordLogin() throws InterruptedException {
+        signInPage.typeText(SignInPage.LOGIN_EMAIL, "");
+        signInPage.typeText(SignInPage.LOGIN_PASSWORD, "");
+        Thread.sleep(2000);
+        signInPage.clickSignIn();
+        Thread.sleep(2000);
     }
+
+    @Test (priority = 2)
+    public void emptyPasswordLogin() throws InterruptedException {
+        signInPage.typeText(SignInPage.LOGIN_EMAIL, "Mary@yopmail.com");
+        signInPage.typeText(SignInPage.LOGIN_PASSWORD, "");
+        Thread.sleep(2000);
+        signInPage.clickSignIn();
+        Thread.sleep(2000);
+    }
+
+    @Test (priority = 3)
+    public void invalidPasswordLogin() throws InterruptedException {
+        signInPage.typeText(SignInPage.LOGIN_EMAIL, "Mary@yopmail.com");
+        signInPage.typeText(SignInPage.LOGIN_PASSWORD, "Incorrect@123");
+        Thread.sleep(2000);
+        signInPage.clickSignIn();
+        Thread.sleep(2000);
+    }
+
+    @Test (priority = 4)
+    public void checkShowAndHideButton() throws InterruptedException {
+        signInPage.typeText(SignInPage.LOGIN_PASSWORD, "TryOne@111");
+        Thread.sleep(2000);
+        signInPage.clickShowButton();
+        Thread.sleep(2000);
+        signInPage.clickHideButton();
+        Thread.sleep(2000);
+        signInPage.clickShowButton();
+        Thread.sleep(2000);
+        signInPage.clickHideButton();
+        Thread.sleep(2000);
+    }
+
+    @Test(priority = 5)
+    public void tickUntickRememberMyCredentials() throws InterruptedException {
+        signInPage.clickRememberMyCredentials();
+        Thread.sleep(2000);
+        signInPage.clickRememberMyCredentials();
+        Thread.sleep(2000);
+        signInPage.clickRememberMyCredentials();
+        Thread.sleep(2000);
+    }
+
+    @Test (priority = 6)
+    public void goToCreateAccPage() throws InterruptedException {
+        signInPage.clickCreateAcc();
+        Thread.sleep(2000);
+    }
+
+    @Test(priority = 6)
+    public void successfulLogin() throws InterruptedException {
+        signInPage.typeText(SignInPage.LOGIN_EMAIL, "Mary@yopmail.com");
+        signInPage.typeText(SignInPage.LOGIN_PASSWORD, "Mary1234*");
+        signInPage.clickSignIn();
+        Thread.sleep(2000);
+    }
+
+
 }
